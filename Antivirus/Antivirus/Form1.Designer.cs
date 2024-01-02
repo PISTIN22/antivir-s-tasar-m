83cc147339508a333b675f3c91880cd0f4f9ef96
Atakan Adalı <atakan_adali@hotmail.com.tr>
2024-01-02 20:53:25 +0300

    Add files via upload

---
+++ Antivirus/Antivirus/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Antivirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Antivirus.Properties.Resources.ANTIVIRUS_LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(7, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(555, 25);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Herhangi Bir Virüs Tespit Edilmemiş Gibi"});
+            this.listBox1.Location = new System.Drawing.Point(7, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(556, 264);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Garamond", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(7, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Klasör Seçiniz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Garamond", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(116, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Virüs Taramasını Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Garamond", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(292, 372);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Virüsleri Kaldır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label1.Location = new System.Drawing.Point(7, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seçilen Klasör :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(7, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tespit Edilen Virüsler : 0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(151)))), ((int)(((byte)(168)))));
+            this.pictureBox2.Image = global::Antivirus.Properties.Resources.IMG_20191223_235845_268_removebg;
+            this.pictureBox2.Location = new System.Drawing.Point(476, 372);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(71, 68);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Antivirus.Properties.Resources.media_1414973106656;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(569, 451);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Antivirus using C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Antivirus/Antivirus/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Antivirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public int viruses = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] search = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.*");
+            progressBar1.Maximum = search.Length;
+            foreach (string item in search)
+            {
+                try
+                {
+                    StreamReader stream = new StreamReader(item);
+                    string read = stream.ReadToEnd();
+                    string[] virus = new string[] { "VIRUSES", "INFECTED", "HACKED", "VIRUS" };
+                    foreach (string st in virus)
+                    {
+                        if (Regex.IsMatch(read, st))
+                        {
+                            viruses += 1;
+                            listBox1.Items.Add(item);
+                            button3.Show();
+                            label2.Text = "Tespit Edilen Virüs : " + viruses.ToString();
+
+                        }
+                        progressBar1.Increment(1);
+                    }
+                }
+                catch
+                {
+                    string read = item;
+                    string[] virus = new string[] { "VIRUSES", "INFECTED", "HACKED","VIRUS" };
+                    foreach (string st in virus)
+                    {
+                        if (Regex.IsMatch(read, st))
+                        {
+                            viruses += 1;
+                            listBox1.Items.Add(item);
+                            button3.Show();
+                            label2.Text = "Tespit Edilen Virüsler : " + viruses.ToString();
+
+                        }
+                        progressBar1.Increment(1);
+
+                    }
+                }
+            }
+            MessageBox.Show("Tarama Tamamlandı");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            label1.Text = "Seçili Klasör : " + folderBrowserDialog1.SelectedPath;
+            viruses = 0;
+            progressBar1.Value = 0;
+            listBox1.Items.Clear();
+            button2.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string removex = listBox1.Text;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            File.Delete(this.listBox1.Text);
+            listBox1.Items.Clear();
+            button3.Hide();
+            MessageBox.Show("Seçili Virüsler Başarıyla Temizlendi");
+            viruses = 0;
+            label2.Text = "Tespit Edilen Virüsler : " + viruses.ToString();
+        }
+    }
+}
